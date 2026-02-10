ea67983a2d12128fe553e6c910b11c0ad93bec58
MohamedAbdelkawy77 <ma6642@fayoum.edu.eg>
2026-02-10 09:53:08 +0200

    ""

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.sakeni"
+    namespace = "com.example.flutter_application_14"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
